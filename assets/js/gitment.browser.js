f0705ef77e878439a3f6827ab89f2268ad7b48cc
Jeremy Zimmerman <stopspazzing@users.noreply.github.com>
2020-03-18 17:08:46 -0700

    gitment tweaks

--- assets/js/gitment.browser.js
+++ assets/js/gitment.browser.js
@@ -3231,7 +3231,7 @@ function ajaxFactory(method) {
     });
     req.open(method, url, true);
 
-    req.setRequestHeader('Accept', 'application/json');
+    req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview, application/vnd.github.html+json');
     if (token) {
       req.setRequestHeader('Authorization', 'token ' + token);
     }
